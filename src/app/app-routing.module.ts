8a835f6ea046f4867f77295efa0e6e3662dab9e8
eduparis.develore <eduparis.developer@gmail.com>
2021-05-31 18:25:51 +0200

    first commit and login design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { IndexComponent} from './pages/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
